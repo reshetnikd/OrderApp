c1822484566cde6aaf8cc69525cd06dd269c3a56
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-28 21:11:01 +0300

    Make Requests with URL Session

--- MenuController.swift
+++ MenuController.swift
@@ -8,19 +8,41 @@
 import Foundation
 
 class MenuController {
-    let baseURL = URL(string: "http://localhost:8080/")
+    let baseURL = URL(string: "http://localhost:8080/")!
     
     func fetchCategories(completion: @escaping (Result<[String], Error>) -> Void) {
-        
+        let categoriesURL = baseURL.appendingPathComponent("categories")
+        let task = URLSession.shared.dataTask(with: categoriesURL) { data, response, error in
+            
+        }
+        task.resume()
     }
     
     func fetchMenuItems(forCategory categoryName: String, completion: @escaping (Result<[MenuItem], Error>) -> Void) {
-        
+        let baseMenuURL = baseURL.appendingPathComponent("menu")
+        var components = URLComponents(url: baseMenuURL, resolvingAgainstBaseURL: true)!
+        components.queryItems = [URLQueryItem(name: "category", value: categoryName)]
+        let menuURL = components.url!
+        let task = URLSession.shared.dataTask(with: menuURL) { data, response, error in
+            
+        }
+        task.resume()
     }
     
     typealias MinutesToPrepare = Int
     
     func submitOrder(forMenuIDs menuIDs: [Int], completion: @escaping (Result<MinutesToPrepare, Error>) -> Void) {
-        
+        let orderURL = baseURL.appendingPathComponent("order")
+        var request = URLRequest(url: orderURL)
+        let data = ["menuIds": menuIDs]
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(data)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            
+        }
+        task.resume()
     }
 }

cc3deedbd02bac93b92839cc78337058c603e610
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-28 21:32:33 +0300

    Basic Table Setup

--- OrderApp/Controllers/CategoryTableViewController.swift
+++ OrderApp/Controllers/CategoryTableViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class CategoryTableViewController: UITableViewController {
+    let menuController = MenuController()
+    var categories = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,29 +19,54 @@ class CategoryTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        menuController.fetchCategories { result in
+            switch result {
+                case .success(let categories):
+                    self.updateUI(with: categories)
+                case .failure(let error):
+                    self.displayError(error, title: "Failed to Fetch Categories")
+            }
+        }
+    }
+    
+    func updateUI(with categories: [String]) {
+        DispatchQueue.main.async {
+            self.categories = categories
+            self.tableView.reloadData()
+        }
+    }
+    
+    func displayError(_ error: Error, title: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categories.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Category", for: indexPath)
 
-        // Configure the cell...
+        configureCell(cell, forCategoryAt: indexPath)
 
         return cell
     }
-    */
+    
+    func configureCell(_ cell: UITableViewCell, forCategoryAt indexPath: IndexPath) {
+        let category = categories[indexPath.row]
+        cell.textLabel?.text = category.capitalized
+    }
 
     /*
     // Override to support conditional editing of the table view.
